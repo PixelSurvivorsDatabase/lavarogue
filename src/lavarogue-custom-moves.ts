5e0b5b0c06a25bc5cceb156a4bcfa9471a8440a0
PixelSurvivorsDatabase <lavalordgaming242@gmail.com>
2026-06-20 17:37:04 -0400

    moves: localize Draco Dance custom move

--- src/lavarogue-custom-moves.ts
+++ src/lavarogue-custom-moves.ts
@@ -11,6 +11,7 @@ import { PokemonType } from "#enums/pokemon-type";
 import { SpeciesId } from "#enums/species-id";
 import { Stat } from "#enums/stat";
 import { SelfStatusMove, StatStageChangeAttr } from "#moves/move";
+import i18next from "i18next";
 
 export const LAVAROGUE_MOVE_IDS: Record<string, MoveId> = {};
 
@@ -25,15 +26,30 @@ function addRuntimeMoveId(name: string): MoveId {
   return id;
 }
 
+function addCustomMoveText(): void {
+  const languages = new Set(["en", i18next.language].filter(Boolean));
+
+  for (const language of languages) {
+    i18next.addResource(language, "move", "dracoDance.name", "Draco Dance");
+    i18next.addResource(
+      language,
+      "move",
+      "dracoDance.effect",
+      "The user performs a mystical draconic dance, boosting its Sp. Atk and Speed stats.",
+    );
+  }
+}
+
 export function initLavaRogueCustomMoves(): void {
+  addCustomMoveText();
+
   const dracoDanceId = addRuntimeMoveId("DRACO_DANCE");
 
   const dracoDance = new SelfStatusMove(dracoDanceId, PokemonType.DRAGON, -1, 20, -1, 0, 9)
     .attr(StatStageChangeAttr, [Stat.SPATK, Stat.SPD], 1, true)
     .danceMove();
 
-  dracoDance.name = "Draco Dance";
-  dracoDance.effect = "The user performs a mystical draconic dance, boosting its Sp. Atk and Speed stats.";
+  dracoDance.localize();
 
   (allMoves as unknown as Array<typeof dracoDance>).push(dracoDance);
 
