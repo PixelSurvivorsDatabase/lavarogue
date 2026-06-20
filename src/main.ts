5aaaa44adb03080275e6258acf42dfc778cf5b04
PixelSurvivorsDatabase <lavalordgaming242@gmail.com>
2026-06-20 16:24:54 -0400

    balance: load LavaRogue Delta Stream buffs

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import "#app/polyfills"; // All polyfills MUST be loaded first for side effects
+import "./lavarogue-delta-stream-buffs"; // LavaRogue custom Delta Stream buffs
 import "#init/init-manifest"; // initializes the manifest, must be done *before* i18n is initialized due to being used for caching
 import "#app/i18n"; // Initializes i18n on import
 
