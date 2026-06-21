adb4da9161997fb0422710fa5d6cf0c775ff17b4
PixelSurvivorsDatabase <lavalordgaming242@gmail.com>
2026-06-20 23:31:01 -0400

    add star dust reward pool init

--- src/init/init.ts
+++ src/init/init.ts
@@ -12,7 +12,7 @@ import { initCatchableSpecies } from "#init/init-catchable-species";
 import { initLavaRogueCelestialAscentBuffs } from "../lavarogue-celestial-ascent-buffs";
 import { initLavaRogueCustomMoves } from "../lavarogue-custom-moves";
 import { initLavaRogueDevTools } from "../lavarogue-devtools";
-import { initLavaRogueStarDust } from "../lavarogue-star-dust";
+import { initLavaRogueStarDust, initLavaRogueStarDustPool } from "../lavarogue-star-dust";
 import { initModifierPools } from "#modifiers/init-modifier-pools";
 import { initModifierTypes } from "#modifiers/modifier-type";
 import { initMoves } from "#moves/move";
@@ -28,6 +28,7 @@ export async function initializeGame(): Promise<void> {
   initModifierTypes();
   initLavaRogueStarDust();
   initModifierPools();
+  initLavaRogueStarDustPool();
   initAchievements();
   initVouchers();
   initStatsKeys();
